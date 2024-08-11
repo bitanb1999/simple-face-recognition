76db1213deb738aedde14868f37ab8645e70f519
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2024-08-12 00:53:30 +0700

    new : add Attendance and Registration upload system.

--- register.py
+++ register.py
@@ -1,3 +1,28 @@
+import os
 import streamlit as st
 
+from uuid import uuid4
+
+
 st.header("Register Employee🙋")
+
+file_name, file_type = 'video1', 'mp4'
+file_path = os.path.join('assets', 'placeholder', f"{file_name}.{file_type}")
+
+file_uploaded = st.file_uploader("Drop a AVI/MP4/MOV/MKV file!", accept_multiple_files=False, type=['avi', 'mp4', 'mov', 'mkv'])
+if file_uploaded is not None:
+    file_name, file_type = file_uploaded.name, file_uploaded.type.split('/')[-1]
+    
+    file_name_new = f"{str(uuid4().hex)}.{file_type}"
+    file_path = os.path.join('static', 'registration', file_name_new)
+
+    with open(file_path, "wb") as f:
+        f.write(file_uploaded.getbuffer())
+    
+    st.success("File saved successfully.")
+else:
+    st.write("You are using a placeholder video. Upload your video to Register.")
+
+video = open(file_path, "rb")
+video = video.read()
+st.video(video, autoplay=True, muted=True)
--- verify.py
+++ verify.py
@@ -1,3 +1,26 @@
+import os
 import streamlit as st
 
+from uuid import uuid4
+
+
 st.header("Verify Attendance🔐")
+
+file_name, file_type = 'img1', 'jpg'
+file_path = os.path.join('assets', 'placeholder', f"{file_name}.{file_type}")
+
+file_uploaded = st.file_uploader("Drop a JPG/PNG file!", accept_multiple_files=False, type=['jpg', 'png'])
+if file_uploaded is not None:
+    file_name, file_type = file_uploaded.name.split('.')
+    
+    file_name_new = f"{str(uuid4().hex)}.{file_type}"
+    file_path = os.path.join('static', 'attendance', file_name_new)
+
+    with open(file_path, "wb") as f:
+        f.write(file_uploaded.getbuffer())
+    
+    st.success("File saved successfully.")
+else:
+    st.write("You are using a placeholder image. Upload your image to Verify Attendance.")
+
+st.image(file_path, caption=f"{file_name}.{file_type}", use_column_width='always', channels='BGR')
