e09ee2d5cfe1af62c0e830566f4b717306018686
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2024-08-16 05:26:14 +0700

    new : try flask

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,20 @@ pg = st.navigation({'Menu': [welcome, verify, register, stored, settings]})
 
 pg.run()
 
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
+
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
+
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
+
+    from app import create_app
+
+    app = create_app()
+    app.run()
