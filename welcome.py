430f8bf4665b6e487818da5a2e0c5e854c279d0e
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2024-08-15 15:22:30 +0700

    fix : update ui

--- welcome.py
+++ welcome.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 
 st.header("Welcome👋")
-st.write(
+
+container = st.container(border=True)
+container.write(
     "Provide simple face recognition with some kind of additional features! For help and instructions, head over to [The Official Repository](https://github.com/fn-hide/simple-face-recognition.git)."
 )
